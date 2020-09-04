b5fdfd1fd71e4c8d96100e83587c6ce5aa423c3a
vakasgnabi <vakasghulamnabi88@gmail.com>
2020-09-04 11:34:57 +0500

    Trello cards mockup using dave's book

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function List({list}){
+    return(
+        <div className="list">
+            <h4 className="list-title">{list.title}</h4>
+            <ul className="list-items">
+                {list.cards.map(item=>(
+                    <li className="list-item" key={item.id}>
+                        {item.title}
+                    </li>
+                ))}
+            </ul>
+            <div className="add-card">Add a card...</div>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Board=({board:{title,lists}})=>{
+    return(
+    <div className="board">
+        <h4 className="board-title">{title}</h4>
+        <div className="board-lists">
+            {lists.map(list=><List key={list.id} list={list} />)}
+        </div>
+    </div>
+    )
+}
+
+let list1={
+   id:0,
+   title:"Phone Features",
+   cards:[
+       {
+           id:0,
+           title: "Subwoofer"
+       },
+       {
+           id:1,
+           title: "Non-porous, washable"
+       },
+       {
+           id:2,
+           title:"Wings"
+       },
+       {
+           id:3,
+           title:"Beveled Bezel"
+       },
+       {
+           id:4,
+           title:"Bezeled Bevel"
+       },
+       {
+           id:5,
+           title:"Seedless"
+       }
+
+    ]
+};
+let list2 = {
+    id: 1,
+    title: 'Lyrics',
+    cards: [
+      {
+        id: 0,
+        title: 'This was a triumph'
+      },
+      {
+        id: 1,
+        title: "I'm making a note here"
+      },
+      {
+        id: 2,
+        title: 'HUGE SUCCESS'
+      },
+      {
+        id: 3,
+        title: "It's hard to overstate my satisfaction"
+      }
+    ]
+  };
+
+let board={
+    title:"A Demo",
+    lists:[list1,list2]
+};
+
+
+ReactDOM.render(<Board board={board} />,document.querySelector('#root'))
+export {Board, board};
